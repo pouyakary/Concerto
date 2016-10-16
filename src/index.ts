82dcc8db73d4c1464a23b1bbd7723b287864f909
Pouya Kary <k@karyfoundation.org>
2016-10-17 00:07:29 +0330

    working index for the first level

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+
+//
+// Copyright 2016 Kary Foundation, Inc.
+//   Author: Pouya Kary <k@karyfoundation.org>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+//
+// ─── IMPORTS ────────────────────────────────────────────────────────────────────
+//
+
+    import * as regulex from 'regulex';
+    import * as regulexToConcertoCompiler from './compilers/regulex-to-concerto';
+
+//
+// ─── MAIN ───────────────────────────────────────────────────────────────────────
+//
+
+    export function compile ( text: string ) {
+        // testing the regex we already have:
+        let ast: blueprints.regulex.IRegExAST;
+        try {
+            ast = regulex.parse( text );
+        }
+        catch ( error ) {
+            throw error;
+        }
+
+        // first level compilation: Regulex AST to Concerto AST
+        let concertoAST = regulexToConcertoCompiler.compile( ast.tree );
+    }
+
+// ────────────────────────────────────────────────────────────────────────────────
