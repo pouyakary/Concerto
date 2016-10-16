0fc7bb9ba98e73bb5f72a27260bd8d0531bc65dd
Pouya Kary <k@karyfoundation.org>
2016-10-16 23:57:16 +0330

    gen kit added

---
+++ src/generators/genkit.ts
@@ -0,0 +1,25 @@
+
+//
+// Copyright 2016 Kary Foundation, Inc.
+//   Author: Pouya Kary <k@karyfoundation.org>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+
+//
+// ─── STATEMENT GENERATOR ────────────────────────────────────────────────────────
+//
+
+    export function generateStatement ( blocksOrBlocks: any, name = 'blocks' ):
+                                        blueprints.block.IStatement {
+        return {
+            name: name,
+            children: ( blocksOrBlocks.length === undefined )?
+                [ blocksOrBlocks ] : blocksOrBlocks
+        }
+    }
+
+// ────────────────────────────────────────────────────────────────────────────────
