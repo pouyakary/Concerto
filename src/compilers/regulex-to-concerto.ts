d105ce9959ff1e1b53d542441bf44f074bcd6a05
Pouya Kary <k@karyfoundation.org>
2016-10-17 00:05:08 +0330

    first level of compilation

---
+++ src/compilers/regulex-to-concerto.ts
@@ -0,0 +1,53 @@
+
+//
+// Copyright 2016 Kary Foundation, Inc.
+//   Author: Pouya Kary <k@karyfoundation.org>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+//
+// ─── IMPORTS ────────────────────────────────────────────────────────────────────
+//
+
+    // tools
+    import * as repeats from '../generators/nodes/repeats';
+
+    // nodes
+    import * as exact from '../generators/nodes/exact'
+
+//
+// ─── EXPORTS ────────────────────────────────────────────────────────────────────
+//
+
+    /** Compiles _Regulex AST_ into _Concerto AST_ */
+    export function compile ( tree: blueprints.regulex.IBaseNode[ ] ) {
+        /** This is where we store the AST at each level */
+        let ast = [ ];
+
+        // this handles each node
+        for ( let node of tree )
+            ast.push( handleOneNode( node ) );
+    }
+
+//
+// ─── HANDLE ONE NODE ────────────────────────────────────────────────────────────
+//
+
+    function handleOneNode ( node: blueprints.regulex.IBaseNode ):
+                                   blueprints.block.IBlock {
+        // firs we handle the block
+        let block;
+        switch ( node.type ) {
+            case 'exact':
+                block = exact.generate( node )
+                break;
+        }
+
+        // then we handle the repeat of the block
+        return repeats.generate( node, block );
+    }
+
+// ────────────────────────────────────────────────────────────────────────────────
