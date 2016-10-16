6a4e348d127b62d873a9347c69e88bcee779254e
Pouya Kary <k@karyfoundation.org>
2016-10-17 00:26:19 +0330

    basic range set added...

--- src/generators/nodes/set.ts
+++ src/generators/nodes/set.ts
@@ -8,3 +8,32 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
 
+//
+// ─── GENERATOR ──────────────────────────────────────────────────────────────────
+//
+
+    export function generate ( node: blueprints.regulex.INodeSet ):
+                                     blueprints.block.IBlock {
+
+        // Simple Range
+        if ( node.ranges.length === 1 && node.chars === '' )
+            return composeRangeBlock( node.ranges[ 0 ] )
+
+
+    }
+
+//
+// ─── RANGE SET BUILDER ──────────────────────────────────────────────────────────
+//
+
+    function composeRangeBlock ( range: string ): blueprints.block.IBlock {
+        return {
+            type: 'range',
+            fields: [
+                {  name: 'start',   value: range[ 0 ] },
+                {  name: 'end',     value: range[ 1 ] }
+            ]
+        }
+    }
+
+// ────────────────────────────────────────────────────────────────────────────────
