9820e041b97cebbffa69a42061191aae643c1bb9
Pouya Kary <k@karyfoundation.org>
2016-10-17 00:04:59 +0330

    exact added

---
+++ src/generators/nodes/exact.ts
@@ -0,0 +1,27 @@
+
+//
+// Copyright 2016 Kary Foundation, Inc.
+//   Author: Pouya Kary <k@karyfoundation.org>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+
+//
+// ─── GENERATOR ──────────────────────────────────────────────────────────────────
+//
+
+    export function generate ( node: blueprints.regulex.IBaseNode ):
+                                     blueprints.block.IBlock {
+        return {
+            type: 'encode',
+            fields: [{
+                name: 'text',
+                value: ( <blueprints.regulex.INodeExact> node ).chars
+            }]
+        }
+    }
+
+// ────────────────────────────────────────────────────────────────────────────────
