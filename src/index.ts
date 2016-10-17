a9817ab72261cebcc3af25a8cbb950559a6502a8
Pouya Kary <k@karyfoundation.org>
2016-10-17 11:46:56 +0330

    final xml generation now fully works....

--- src/index.ts
+++ src/index.ts
@@ -15,12 +15,13 @@
     import * as regulex                     from 'regulex'
     import * as regulexToConcertoCompiler   from './compilers/level1'
     import * as concertoToRecarrCompiler    from './compilers/level2'
+    import * as recarrToQuartetXMLCompiler  from './compilers/level3'
 
 //
 // ─── MAIN ───────────────────────────────────────────────────────────────────────
 //
 
-    export function compile ( text: string ) {
+    export function compile ( text: string, debug = false ) {
         // testing the regex we already have:
         let regulexAST: blueprints.regulex.IRegExAST
         try {
@@ -30,18 +31,34 @@
             throw error
         }
 
-        console.log('--- Regulex AST --------------------------')
-        console.log( regulexAST )
+        if ( debug ) {
+            console.log('--- Regulex AST --------------------------')
+            console.log( regulexAST )
+        }
 
         // first level compilation: Regulex AST to Concerto AST
         let concertoAST = regulexToConcertoCompiler.compile( regulexAST.tree )
-        console.log('--- Concerto AST -------------------------')
-        console.log( concertoAST )
+        if ( debug ) {
+            console.log('--- Concerto AST -------------------------')
+            console.log( concertoAST )
+        }
 
         // then we compile the concerto ast to recursive array ast
         let recarrAST = concertoToRecarrCompiler.compile( concertoAST )
-        console.log('--- Concerto Recursive Array AST ---------')
-        console.log( recarrAST )
+        if ( debug ) {
+            console.log('--- Concerto Recursive Array AST ---------')
+            console.log( recarrAST )
+        }
+
+        // at the end we compile the recursive array into the Quartet XML
+        let quartetXML = recarrToQuartetXMLCompiler.compile( recarrAST )
+        if ( debug ) {
+            console.log('--- Quartet Block XML --------------------')
+            console.log( quartetXML )
+        }
+
+        // done!
+        return quartetXML
     }
 
 // ────────────────────────────────────────────────────────────────────────────────
