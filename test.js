700d83582265f67809b151a3341890a8279d05e6
Pouya Kary <k@karyfoundation.org>
2017-08-21 18:38:39 +0430

    formatted a lot

--- test.js
+++ test.js
@@ -1,33 +1,57 @@
 
-const concerto = require( './bin/index.js' )
-const regulex = require( 'regulex' )
-const fs = require( 'fs' )
-const path = require( 'path' )
+//
+// ─── INCLUDES ───────────────────────────────────────────────────────────────────
+//
 
-function getDesktop ( ) {
-    return path.join( ( process.env.HOME || process.env.USERPROFILE ), 'Desktop' )
-}
+    const concerto  = require( './bin/index.js' )
+    const regulex   = require( 'regulex' )
+    const fs        = require( 'fs' )
+    const path      = require( 'path' )
 
-// testing the regex we already have:
-let regulexAST
-try {
-    regulexAST = regulex.parse( '((?:let|var|const)) +(?:([a-zA-Z_][0-9a-zA-Z_]+)|\{[ \t]*([a-zA-Z_][0-9a-zA-Z_]+)[ \t]*(?:,[ \t]*([a-zA-Z_][0-9a-zA-Z_]+)[ \t])*\}) *[,;\=]' )
-    //console.log( '--- Regulex AST --------------------------' )
-    //console.log( regulexAST )
-}
-catch ( error ) {
-    throw error
-}
+//
+// ─── TEST CASES ─────────────────────────────────────────────────────────────────
+//
 
-const xml = concerto.compile( regulexAST )
+    // const testRegExp = /((?:let|var|const)) +(?:([a-zA-Z_][0-9a-zA-Z_]+)|\{[ \t]*([a-zA-Z_][0-9a-zA-Z_]+)[ \t]*(?:,[ \t]*([a-zA-Z_][0-9a-zA-Z_]+)[ \t])*\}) *[,;\=]/
+    const testRegExp = /[\s0-9]/
 
-////console.log( '--- Quartet File -------------------------' )
+//
+// ─── GET DESKTOP ────────────────────────────────────────────────────────────────
+//
 
-//console.log( `{"workspaceXML":"${ xml }","playgroundText":""}` )
+    function getDesktop ( ) {
+        return path.join( ( process.env.HOME || process.env.USERPROFILE ), 'Desktop' )
+    }
 
-fs.writeFileSync(
-    path.join( getDesktop( ), 'a.orchestra' ), `{"version":"1.0","workspaceXML":"${ xml }","flags":"gm","playgroundText":""}`)
+//
+// ─── AST ────────────────────────────────────────────────────────────────────────
+//
 
-console.log('Test RegExp compiled and saved as "a.orchestra" on desktop.')
+    let regulexAST
+    try {
+        regulexAST = regulex.parse( testRegExp.source )
+    }
+    catch ( error ) {
+        throw error
+    }
 
-{ }
+//
+// ─── XML ────────────────────────────────────────────────────────────────────────
+//
+
+    const blocklyXML = concerto.compile( regulexAST )
+
+//
+// ─── SAVING FILE ────────────────────────────────────────────────────────────────
+//
+
+    const orchestraFileString =
+        `{"version":"1.0","workspaceXML":"${ blocklyXML }","playgroundText":""}`
+    const pathToFile =
+        path.join( getDesktop( ), 'a.orchestra' )
+
+    fs.writeFileSync( pathToFile, orchestraFileString )
+
+    console.log('Test RegExp compiled and saved as "a.orchestra" on desktop.')
+
+// ────────────────────────────────────────────────────────────────────────────────
